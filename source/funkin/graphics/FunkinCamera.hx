bfaae3d2827c70386e40fdd923e9aa923c1483a5
AbnormalPoof <abnormalpoof@icloud.com>
2025-11-18 08:01:46 -0600

    Add support for unsupported blend modes in `FunkinCamera`

--- source/funkin/graphics/FunkinCamera.hx
+++ source/funkin/graphics/FunkinCamera.hx
@@ -8,14 +8,13 @@ import flixel.math.FlxMatrix;
 import flixel.math.FlxRect;
 import flixel.system.FlxAssets.FlxShader;
 import funkin.graphics.shaders.RuntimeCustomBlendShader;
-import funkin.graphics.framebuffer.BitmapDataUtil;
 import funkin.graphics.framebuffer.FixedBitmapData;
 import openfl.Lib;
 import openfl.display.BitmapData;
 import openfl.display.BlendMode;
 import openfl.display3D.textures.TextureBase;
 import openfl.filters.BitmapFilter;
-import openfl.filters.ShaderFilter;
+import animate.internal.RenderTexture;
 
 /**
  * A FlxCamera with additional powerful features:
@@ -33,36 +32,29 @@ import openfl.filters.ShaderFilter;
 @:access(flixel.graphics.frames.FlxFrame)
 class FunkinCamera extends FlxCamera
 {
-  final grabbed:Array<BitmapData> = [];
-  final texturePool:Array<TextureBase> = [];
+  public var id:String = 'unknown';
 
-  final bgTexture:TextureBase;
-  final bgBitmap:BitmapData;
-  final bgFrame:FlxFrame;
+  var _blendShader:RuntimeCustomBlendShader;
+  var _backgroundFrame:FlxFrame;
 
-  final customBlendShader:RuntimeCustomBlendShader;
-  final customBlendFilter:ShaderFilter;
+  var _appliedFilters:Bool = false;
 
-  var filtersApplied:Bool = false;
-  var bgItemCount:Int = 0;
+  var _shouldDraw:Bool = true;
 
-  public var shouldDraw:Bool = true;
+  var _blendRenderTexture:Null<RenderTexture>;
+  var _backgroundRenderTexture:Null<RenderTexture>;
 
-  // Used to identify the camera during debugging.
-  final id:String = 'unknown';
+  var _backgroundBitmap:Null<BitmapData>;
 
   @:nullSafety(Off)
   public function new(id:String = 'unknown', x:Int = 0, y:Int = 0, width:Int = 0, height:Int = 0, zoom:Float = 0)
   {
     super(x, y, width, height, zoom);
     this.id = id;
-    bgTexture = @:nullSafety(Off) pickTexture(width, height);
-    bgBitmap = FixedBitmapData.fromTexture(bgTexture);
-    bgFrame = new FlxFrame(new FlxGraphic('', null));
-    bgFrame.parent.bitmap = bgBitmap;
-    bgFrame.frame = new FlxRect();
-    customBlendShader = new RuntimeCustomBlendShader();
-    customBlendFilter = new ShaderFilter(customBlendShader);
+    _backgroundFrame = new FlxFrame(new FlxGraphic('', null));
+    _backgroundFrame.frame = new FlxRect();
+
+    _blendShader = new RuntimeCustomBlendShader();
   }
 
   /**
@@ -74,140 +66,133 @@ class FunkinCamera extends FlxCamera
    * and the camera's filters will be disabled until the beginning of the next frame
    * @param isolate if this is `true`, sprites to be rendered will only be rendered to the grabbed bitmap,
    * and the grabbed bitmap will not include any previously rendered sprites
+   * @param clearScreen if this is `true`, the screen will be cleared before rendering
    * @return the grabbed bitmap data
    */
-  public function grabScreen(applyFilters:Bool, isolate:Bool = false):Null<BitmapData>
+  public function grabScreen(applyFilters:Bool = false, isolate:Bool = false, clearScreen:Bool = false):Null<BitmapData>
   {
-    final texture = pickTexture(width, height);
-    final bitmap = FixedBitmapData.fromTexture(texture);
-    if (bitmap != null)
+    if (_backgroundBitmap == null)
     {
-      squashTo(bitmap, applyFilters, isolate);
-      grabbed.push(bitmap);
-    }
-    return bitmap;
-  }
+      var texture:Null<TextureBase> = pickTexture(width, height);
+      if (texture == null) return null;
 
-  /**
-   * Applies the filter immediately to the camera. This will be done independently from
-   * the camera's filters. This method can only be called after the first `grabScreen`
-   * in the frame.
-   * @param filter the filter
-   */
-  public function applyFilter(filter:BitmapFilter):Void
-  {
-    if (grabbed.length == 0)
-    {
-      FlxG.log.error('grab screen before you can apply a filter!');
-      return;
-    }
-    BitmapDataUtil.applyFilter(bgBitmap, filter);
-  }
-
-  function squashTo(bitmap:BitmapData, applyFilters:Bool, isolate:Bool, clearScreen:Bool = false):Void
-  {
-    if (applyFilters && isolate)
-    {
-      FlxG.log.error('cannot apply filters while isolating!');
-    }
-    if (filtersApplied && applyFilters)
-    {
-      FlxG.log.warn('filters already applied!');
-    }
-    static final matrix = new FlxMatrix();
-
-    // resize the background bitmap if needed
-    if (bgTexture.__width != width || bgTexture.__height != height)
-    {
-      BitmapDataUtil.resizeTexture(bgTexture, width, height);
-      bgBitmap.__resize(width, height);
-      bgFrame.parent.bitmap = bgBitmap;
+      _backgroundBitmap = FixedBitmapData.fromTexture(texture);
     }
 
-    // grab the bitmap
-    renderSkipping(isolate ? bgItemCount : 0);
-    bitmap.fillRect(bitmap.rect, 0);
-    matrix.setTo(1, 0, 0, 1, flashSprite.x, flashSprite.y);
-    if (applyFilters)
+    if (_backgroundBitmap != null)
     {
-      bitmap.draw(flashSprite, matrix);
-      @:nullSafety(Off) // TODO: Remove this once openfl.display.Sprite has been null safed.
-      flashSprite.filters = null;
-      filtersApplied = true;
-    }
-    else
-    {
-      final tmp = flashSprite.filters;
-      @:nullSafety(Off)
-      flashSprite.filters = null;
-      bitmap.draw(flashSprite, matrix);
-      flashSprite.filters = tmp;
-    }
-
-    if (!isolate)
-    {
-      // also copy to the background bitmap
-      bgBitmap.fillRect(bgBitmap.rect, 0);
-      bgBitmap.draw(bitmap);
-    }
-
-    if (clearScreen)
-    {
-      // clear graphics data
-      super.clearDrawStack();
-      canvas.graphics.clear();
-    }
-
-    // render the background bitmap
-    bgFrame.frame.set(0, 0, width, height);
-    matrix.setTo(viewWidth / width, 0, 0, viewHeight / height, viewMarginLeft, viewMarginTop);
-    drawPixels(bgFrame, matrix);
-
-    // count background draw items for future isolation
-    bgItemCount = 0;
-    {
-      var item = _headOfDrawStack;
-      while (item != null)
+      if (applyFilters && isolate)
       {
-        item = item.next;
-        bgItemCount++;
+        FlxG.log.error('cannot apply filters while isolating!');
       }
-    }
-  }
+      if (_appliedFilters && applyFilters)
+      {
+        FlxG.log.warn('filters already applied!');
+      }
 
-  function renderSkipping(count:Int):Void
-  {
-    var item = _headOfDrawStack;
-    while (item != null)
-    {
-      if (--count < 0) item.render(this);
-      item = item.next;
+      var matrix:FlxMatrix = new FlxMatrix();
+      matrix.setTo(1, 0, 0, 1, flashSprite.x, flashSprite.y);
+
+      this.render();
+
+      if (applyFilters)
+      {
+        _backgroundBitmap.draw(flashSprite, matrix, true);
+        @:nullSafety(Off) // TODO: Remove this once openfl.display.Sprite has been null safed.
+        flashSprite.filters = null;
+        _appliedFilters = true;
+      }
+      else
+      {
+        var _tmpFilters:Array<BitmapFilter> = flashSprite.filters.copy();
+        @:nullSafety(Off)
+        flashSprite.filters = null;
+        _backgroundBitmap.draw(flashSprite, matrix, true);
+        flashSprite.filters = _tmpFilters;
+      }
+
+      if (clearScreen)
+      {
+        // clear graphics data
+        super.clearDrawStack();
+        canvas.graphics.clear();
+      }
+
+      _backgroundFrame.frame.set(0, 0, width, height);
     }
+
+    return _backgroundBitmap;
   }
 
   override function drawPixels(?frame:FlxFrame, ?pixels:BitmapData, matrix:FlxMatrix, ?transform:ColorTransform, ?blend:BlendMode, ?smoothing:Bool = false,
       ?shader:FlxShader):Void
   {
-    if (!shouldDraw) return;
+    if (!_shouldDraw) return;
 
     if ( switch blend
       {
-        case DARKEN | HARDLIGHT | LIGHTEN | OVERLAY: true;
-        case _: false;
+        case DARKEN | HARDLIGHT #if !desktop | LIGHTEN #end | OVERLAY | DIFFERENCE: true;
+        default: false;
       })
     {
-      // squash the screen
-      grabScreen(false);
-      // render without blend
-      super.drawPixels(frame, pixels, matrix, transform, null, smoothing, shader);
-      // get the isolated bitmap
-      final isolated = grabScreen(false, true);
-      // apply fullscreen blend
-      customBlendShader.blendSwag = blend;
-      @:nullSafety(Off) // I hope this doesn't cause issues
-      customBlendShader.sourceSwag = isolated;
-      customBlendShader.updateViewInfo(FlxG.width, FlxG.height, this);
-      applyFilter(customBlendFilter);
+      var background:Null<BitmapData> = grabScreen(false, false, true);
+      var frameMatrix:FlxMatrix = new FlxMatrix();
+      frameMatrix.copyFrom(matrix);
+
+      @:nullSafety(Off) {
+        if (_blendRenderTexture == null)
+        {
+          _blendRenderTexture = new RenderTexture(this.width, this.height);
+        }
+
+        _blendRenderTexture.init(this.width, this.height);
+        _blendRenderTexture.drawToCamera((camera, matrix) -> {
+          var pivotX:Float = width / 2;
+          var pivotY:Float = height / 2;
+
+          matrix.copyFrom(frameMatrix);
+          matrix.translate(-pivotX, -pivotY);
+          matrix.scale(this.zoom, this.zoom);
+          matrix.translate(pivotX, pivotY);
+          camera.drawPixels(frame, pixels, matrix, transform, null, smoothing, shader);
+        });
+        _blendRenderTexture.render();
+      }
+      @:nullSafety(Off)
+      if (background == null || _blendRenderTexture.graphic.bitmap == null)
+      {
+        FlxG.log.error('Failed to get bitmap for blending!');
+        super.drawPixels(frame, pixels, matrix, transform, blend, smoothing, shader);
+        return;
+      }
+
+      @:nullSafety(Off)
+      _blendShader.sourceSwag = _blendRenderTexture.graphic.bitmap;
+
+      @:nullSafety(Off)
+      _blendShader.backgroundSwag = background;
+
+      _blendShader.blendSwag = blend;
+      _blendShader.updateViewInfo(width, height, this);
+
+      @:nullSafety(Off)
+      _backgroundFrame.parent.bitmap = _blendRenderTexture.graphic.bitmap;
+
+      if (_backgroundRenderTexture == null)
+      {
+        _backgroundRenderTexture = new RenderTexture(this.width, this.height);
+      }
+
+      _backgroundRenderTexture.init(this.width, this.height);
+      _backgroundRenderTexture.drawToCamera((camera, matrix) -> {
+        camera.zoom = this.zoom;
+        camera.drawPixels(_backgroundFrame, null, matrix, canvas.transform.colorTransform, null, smoothing, _blendShader);
+      });
+
+      _backgroundRenderTexture.render();
+      super.drawPixels(_backgroundRenderTexture.graphic.imageFrame.frame, null, new FlxMatrix(), null, null, smoothing, null);
+
+      this.setScale(1, 1);
     }
     else
     {
@@ -218,22 +203,26 @@ class FunkinCamera extends FlxCamera
   override function destroy():Void
   {
     super.destroy();
-    disposeTextures();
+
+    if (_blendRenderTexture != null)
+    {
+      _blendRenderTexture.destroy();
+      _blendRenderTexture = null;
+    }
+
+    if (_backgroundRenderTexture != null)
+    {
+      _backgroundRenderTexture.destroy();
+      _backgroundRenderTexture = null;
+    }
   }
 
   override function clearDrawStack():Void
   {
     super.clearDrawStack();
-    // also clear grabbed bitmaps
-    for (bitmap in grabbed)
-    {
-      texturePool.push(bitmap.__texture);
-      bitmap.dispose(); // this doesn't release the texture
-    }
-    grabbed.clear();
+
     // clear filters applied flag
-    filtersApplied = false;
-    bgItemCount = 0;
+    _appliedFilters = false;
   }
 
   function pickTexture(width:Int, height:Int):Null<TextureBase>
@@ -241,30 +230,7 @@ class FunkinCamera extends FlxCamera
     // zero-sized textures will be problematic
     width = width < 1 ? 1 : width;
     height = height < 1 ? 1 : height;
-    if (texturePool.length > 0)
-    {
-      final res = texturePool.pop();
-      if (res != null) BitmapDataUtil.resizeTexture(res, width, height);
-      else
-        trace('huh? why is this null? $texturePool');
-      return res;
-    }
-    return Lib.current.stage.context3D.createTexture(width, height, BGRA, true);
-  }
 
-  function disposeTextures():Void
-  {
-    for (bitmap in grabbed)
-    {
-      bitmap.dispose();
-    }
-    grabbed.clear();
-    for (texture in texturePool)
-    {
-      texture.dispose();
-    }
-    texturePool.resize(0);
-    bgTexture.dispose();
-    bgBitmap.dispose();
+    return Lib.current.stage.context3D.createTexture(width, height, BGRA, true);
   }
 }
